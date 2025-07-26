8ffc640b5977a65752183d2101c8df76782e3600
takt1232 <jericho.gabotero6557@gmail.com>
2025-07-26 19:18:38 +0800

    implemented delete post

--- Gemfile
+++ Gemfile
@@ -63,3 +63,4 @@ group :test do
 end
 
 gem "devise", "~> 4.9"
+gem "htmlbeautifier"
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,4 @@
 class PagesController < ApplicationController
-  before_action :authenticate_user!, only: [ :home ]
-
   def index
   end
 end
