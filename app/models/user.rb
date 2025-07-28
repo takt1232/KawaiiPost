db5baeb57d661a360b621e9dfd7a6ec94416d002
takt1232 <jericho.gabotero6557@gmail.com>
2025-07-28 14:25:28 +0800

    hope another fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  after_create_commit :broadcast_stats_update
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
